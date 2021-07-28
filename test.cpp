d036d1843d7bdf77dec08473717ba0f722706376
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-28 15:43:13 +0200

    compiles, but runs with exit code 1

--- test.cpp
+++ test.cpp
@@ -2,8 +2,22 @@
 // Created by studio25 on 25.07.2021.
 //
 #include <SFML/Graphics.hpp>
-int main(){
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+#include <iostream>
+int main() {
+  std::cout<<"lel";
+  sf::Window window;
+  window.create(sf::VideoMode(800, 600), "My window");
+
+  while (window.isOpen()) {
+    // check all the window's events that were triggered since the last
+    // iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
 
   return 0;
 }
