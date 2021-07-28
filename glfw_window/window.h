e0841426f753e19c791ebb5be293bab4f75e3861
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-28 19:16:01 +0200

    new Window class

---
+++ glfw_window/window.h
@@ -0,0 +1,17 @@
+//
+// Created by studio25 on 28.07.2021.
+//
+
+#ifndef BLOCK_V2_GLFW_WINDOW_WINDOW_H_
+#define BLOCK_V2_GLFW_WINDOW_WINDOW_H_
+
+#include <GLFW/glfw3.h>
+
+class Window {
+public:
+  Window();
+
+private:
+};
+
+#endif // BLOCK_V2_GLFW_WINDOW_WINDOW_H_
