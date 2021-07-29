94fa16cc7c884993292aaea133335c39c6aa0e88
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 20:21:13 +0200

    empty class

---
+++ bots/empty.cpp
@@ -0,0 +1,7 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "empty.h"
+Empty::Empty(const Coord &position) : position_(position) {}
+Empty::Empty() {}
---
+++ bots/empty.h
@@ -0,0 +1,17 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_EMPTY_H_
+#define BLOCK_V2_BOTS_EMPTY_H_
+#include "bot.h"
+class Empty: public Bot{
+public:
+  Empty(const Coord &position);
+  Empty();
+
+private:
+  Coord position_;
+};
+
+#endif // BLOCK_V2_BOTS_EMPTY_H_
