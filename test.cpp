3e7c32b7250f08240377ea23d03f9588e9277b2a
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 13:49:39 +0200

    both windows test

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "glfw_window/window.h"
+#include <SFML/Window.hpp>
 #include <iostream>
 int main() {
 
@@ -10,12 +11,21 @@ int main() {
   std::cout<<"lel";
   Window cos;
 
-  for (int i = 0; i < 20; ++i)
-  {
-    printf("lol  \n");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+  sf::Window window(sf::VideoMode(800, 600), "My window");
 
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
   }
+
 
   return 0;
 }
