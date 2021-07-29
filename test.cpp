9092bc87a88e39a79472a95979acb3a857891bb9
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 14:51:32 +0200

    new window api

--- test.cpp
+++ test.cpp
@@ -3,28 +3,13 @@
 //
 
 #include "glfw_window/window.h"
-#include <SFML/Window.hpp>
-#include <iostream>
+#include "sfml_window/window.h"
 int main() {
 
 
   std::cout<<"lel";
-  Window cos;
+  sfml::Window sin;
 
-  sf::Window window(sf::VideoMode(800, 600), "My window");
-
-  // run the program as long as the window is open
-  while (window.isOpen())
-  {
-    // check all the window's events that were triggered since the last iteration of the loop
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      // "close requested" event: we close the window
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-  }
 
 
   return 0;
