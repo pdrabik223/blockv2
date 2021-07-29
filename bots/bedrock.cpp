fb44c7f3695b271f9004dccef0055d67199420de
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 20:21:21 +0200

    bedrock class

---
+++ bots/bedrock.cpp
@@ -0,0 +1,7 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "bedrock.h"
+Bedrock::Bedrock(const Coord &position) : position_(position) {}
+Bedrock::Bedrock() {}
---
+++ bots/bedrock.h
@@ -0,0 +1,17 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_BEDROCK_H_
+#define BLOCK_V2_BOTS_BEDROCK_H_
+#include "bot.h"
+class Bedrock: public Bot {
+public:
+  Bedrock(const Coord &position);
+  Bedrock();
+
+private:
+  Coord position_;
+};
+
+#endif // BLOCK_V2_BOTS_BEDROCK_H_
