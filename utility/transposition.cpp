55c8a1d001f2f82d0a12680c0505f6e9d61a7d0b
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 23:09:32 +0200

    transposition class maybe it will solve all the problems with v1

---
+++ utility/transposition.cpp
@@ -0,0 +1,16 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "transposition.h"
+
+Transposition::Transposition(){}
+void Transposition::PushTransposition(TranspositionType target) {
+  transposition_.emplace_back(target, Bot());
+}
+void Transposition::PushTransposition(TranspositionType target, Bot info) {
+  transposition_.emplace_back(target, info);
+}
+Bot &Transposition::Collapse(Bot &target) {
+  return target;
+}
---
+++ utility/transposition.h
@@ -0,0 +1,29 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_UTILITY_TRANSPOSITION_H_
+#define BLOCK_V2_UTILITY_TRANSPOSITION_H_
+
+#include "../bots/bots_main.h"
+#include <vector>
+enum class TranspositionType {
+  NONE,
+  CREATE,
+  MOVE_TO,
+  TURN,
+  SIZE
+
+};
+
+class Transposition {
+public:
+  Transposition();
+  void PushTransposition(TranspositionType target);
+  void PushTransposition(TranspositionType target, Bot info);
+  Bot& Collapse(Bot& target);
+private:
+  std::vector<std::pair<TranspositionType, Bot>> transposition_;
+};
+
+#endif // BLOCK_V2_UTILITY_TRANSPOSITION_H_
