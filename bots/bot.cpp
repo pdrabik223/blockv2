50516bbfd2a21e803b2ccccde184ed3141b6b117
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 18:08:37 +0200

    mew bot template class

---
+++ bots/bot.cpp
@@ -0,0 +1,11 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "bot.h"
+Coord::Coord(unsigned int x, unsigned int y) : x(x), y(y) {}
+Coord::Coord(): x(0), y(0) {}
+bool Coord::operator==(const Coord &rhs) const {
+  return x == rhs.x && y == rhs.y;
+}
+bool Coord::operator!=(const Coord &rhs) const { return !(rhs == *this); }
---
+++ bots/bot.h
@@ -0,0 +1,24 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_BOT_H_
+#define BLOCK_V2_BOTS_BOT_H_
+#include "../utility/coord.h"
+enum class BotType{
+
+
+};
+
+class Bot {
+public:
+  Bot():position_(){} // std::array needs no-param constructor
+protected:
+
+
+private:
+Coord position_;
+
+};
+
+#endif // BLOCK_V2_BOTS_BOT_H_
