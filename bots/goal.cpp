5d8077b64e95134268ead63835408a3835c0038e
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 20:20:44 +0200

    goal class

---
+++ bots/goal.cpp
@@ -0,0 +1,7 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "goal.h"
+Goal::Goal(const Coord &position) : position_(position) {}
+Goal::Goal() {}
---
+++ bots/goal.h
@@ -0,0 +1,17 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_GOAL_H_
+#define BLOCK_V2_BOTS_GOAL_H_
+#include "bot.h"
+class Goal: public Bot {
+public:
+  Goal(const Coord &position);
+  Goal();
+
+private:
+  Coord position_;
+};
+
+#endif // BLOCK_V2_BOTS_GOAL_H_
