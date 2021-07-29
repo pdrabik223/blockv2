a05973e99ec8afbf48830e2dca58425a0fe9f36c
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 18:14:09 +0200

    coord functions definitions

--- utility/coord.cpp
+++ utility/coord.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "coord.h"
+Coord::Coord(unsigned int x, unsigned int y) : x(x), y(y) {}
+Coord::Coord(): x(0), y(0) {}
+bool Coord::operator==(const Coord &rhs) const {
+  return x == rhs.x && y == rhs.y;
+}
+bool Coord::operator!=(const Coord &rhs) const { return !(rhs == *this); }
