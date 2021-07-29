0dac550f310aeef47d27b12d7c434f17b25519a1
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 20:21:30 +0200

    basic class

---
+++ bots/basic.cpp
@@ -0,0 +1,7 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "basic.h"
+Basic::Basic(const Coord &position) : position_(position) {}
+Basic::Basic() {}
---
+++ bots/basic.h
@@ -0,0 +1,19 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_BASIC_H_
+#define BLOCK_V2_BOTS_BASIC_H_
+
+#include "bot.h"
+class Basic :Bot{
+
+public:
+  Basic(const Coord &position);
+  Basic();
+
+private:
+  Coord position_;
+};
+
+#endif // BLOCK_V2_BOTS_BASIC_H_
