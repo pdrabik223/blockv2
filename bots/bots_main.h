41d9f53ade32ed3c0620b492b673b1974ae3ab66
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 23:10:07 +0200

    include file for cells/bots

---
+++ bots/bots_main.h
@@ -0,0 +1,19 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_BOTS_MAIN_H_
+#define BLOCK_V2_BOTS_BOTS_MAIN_H_
+
+///  include file
+#include "basic.h"
+#include "bedrock.h"
+#include "enemy.h"
+#include "engine.h"
+#include "factory.h"
+#include "goal.h"
+#include "turn.h"
+#include "bot.h"
+#include "empty.h"
+
+#endif // BLOCK_V2_BOTS_BOTS_MAIN_H_
