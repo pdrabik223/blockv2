dbbfe2f8704214fa1d5ff286076801e005dda167
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 20:20:37 +0200

    tp class

---
+++ bots/tp.cpp
@@ -0,0 +1,7 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#include "tp.h"
+Tp::Tp(const Coord &position) : position_(position) {}
+Tp::Tp() {}
---
+++ bots/tp.h
@@ -0,0 +1,17 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2_BOTS_TP_H_
+#define BLOCK_V2_BOTS_TP_H_
+#include "bot.h"
+class Tp : public Bot{
+public:
+  Tp(const Coord &position);
+  Tp();
+
+private:
+  Coord position_;
+};
+
+#endif // BLOCK_V2_BOTS_TP_H_
