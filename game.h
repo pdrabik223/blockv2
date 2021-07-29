237405c9766a10fa33f65ec35e94cd4314b90d4f
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-29 23:09:42 +0200

    main engine class

---
+++ game.h
@@ -0,0 +1,18 @@
+//
+// Created by studio25 on 29.07.2021.
+//
+
+#ifndef BLOCK_V2__GAME_H_
+#define BLOCK_V2__GAME_H_
+#include "bots_main.h"
+#include <vector>
+
+class Game {
+
+
+private:
+  std::vector<Bot> plane_;
+
+};
+
+#endif // BLOCK_V2__GAME_H_
