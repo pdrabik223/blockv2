9d22a24dbb998899163673f3ea262d328c2ad3fd
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 01:49:33 +0200

    new button class

---
+++ sfml_window/button.h
@@ -0,0 +1,14 @@
+//
+// Created by studio25 on 01.08.2021.
+//
+
+#ifndef BLOCK_V2_SFML_WINDOW_BUTTON_H_
+#define BLOCK_V2_SFML_WINDOW_BUTTON_H_
+
+class Button {
+
+
+
+};
+
+#endif // BLOCK_V2_SFML_WINDOW_BUTTON_H_
