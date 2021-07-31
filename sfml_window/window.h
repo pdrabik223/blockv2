0eaa2c61cc48e156c10bbd274a069c383ac4c6c1
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-31 21:49:03 +0200

    window class update

--- sfml_window/window.h
+++ sfml_window/window.h
@@ -5,9 +5,12 @@
 #ifndef BLOCK_V2_SFML_WINDOW_WINDOW_H_
 #define BLOCK_V2_SFML_WINDOW_WINDOW_H_
 
+#include "../bots/bots_main.h"
+#include <SFML/Graphics/Shape.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
 #include <thread>
+
 namespace sfml {
 /// creates window object in separate tread
 /// window update is done by pushing special event on to event stack
@@ -20,6 +23,7 @@ private:
   void ThMainLoop();
 
 protected:
+  std::vector<sf::Shape> images_; // the size of  images is static
   std::thread *window_thread_;
   sf::Event event_;
 
