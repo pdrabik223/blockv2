5e61c945179311753aa3471edf94304c321db942
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 01:07:21 +0200

    context class a.k.a. the solution to the multiple screens problem

---
+++ sfml_window/context.cpp
@@ -0,0 +1,5 @@
+//
+// Created by studio25 on 01.08.2021.
+//
+
+#include "context.h"
---
+++ sfml_window/context.h
@@ -0,0 +1,20 @@
+//
+// Created by studio25 on 01.08.2021.
+//
+
+#ifndef BLOCK_V2_SFML_WINDOW_CONTEXT_H_
+#define BLOCK_V2_SFML_WINDOW_CONTEXT_H_
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <coord.h>
+namespace sfml_window {
+class Context {
+public:
+
+  virtual void DisplayToWindow(sf::RenderWindow &window) = 0;
+  virtual void HandleEvent(sf::Event& event, Coord mouse_position) = 0;
+
+};
+} // namespace sfml_window
+#endif // BLOCK_V2_SFML_WINDOW_CONTEXT_H_
