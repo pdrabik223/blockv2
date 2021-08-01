34b75957a41a8481d329e93fbf36af3f516569cc
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 03:06:27 +0200

    button class functions declaration

--- sfml_window/button.h
+++ sfml_window/button.h
@@ -5,10 +5,19 @@
 #ifndef BLOCK_V2_SFML_WINDOW_BUTTON_H_
 #define BLOCK_V2_SFML_WINDOW_BUTTON_H_
 
+#include "../sfml/include/SFML/Graphics/RenderWindow.hpp"
+#include "../sfml/include/SFML/Window/Event.hpp"
+#include <rect.h>
+namespace sfml_window {
 class Button {
 
+public:
+  virtual bool DetectHover(const Coord &press_point) = 0;
+  virtual bool DetectPress(const Coord &press_point,sf::Event& event) = 0;
+  virtual void DrawToWindow(sf::RenderWindow &window) = 0;
 
-
+protected:
+  Rect position_;
 };
-
+} // namespace sfml
 #endif // BLOCK_V2_SFML_WINDOW_BUTTON_H_
