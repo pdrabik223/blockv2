a664e7e634c7b02cd764535aa09bfdf0479c4a2e
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 13:46:13 +0200

    added color enum and array the idea is that gui can have multiple color versions

--- sfml_window/context.h
+++ sfml_window/context.h
@@ -8,13 +8,42 @@
 #include "../sfml/include/SFML/Graphics/RenderWindow.hpp"
 #include "../sfml/include/SFML/Window/Event.hpp"
 #include "../utility/coord.h"
-
+#include <array>
 namespace sfml_window {
+
+/// all colors across whole platform must be unified
+/// all menus, and graphical elements must take one of these pre-defined colors
+/// this approach assures easy re-coloring of whole platform by simply editing one vector
+enum class GuiColor{
+  MENU_PRIMARY_COLOR,
+  MENU_SECONDARY_COLOR,
+  MENU_TERTIARY_COLOR,
+  MENU_BACKGROUND_COLOR,
+  /// \default red
+  /// \info to indicate dangerous behavior
+  DANGER_COLOR,
+  /// \default yellow
+  /// \info  to indicate not that important stuff
+  WARNING_COLOR,
+  /// \default blue
+  /// \info  to inform player about something
+  INFORMATIVE_COLOR,
+  /// \default green
+  /// \info  stuff that is safe to touch
+  SAFE_COLOR,
+  SIZE
+};
+
 class Context {
 public:
 
+  virtual void LoadColors()=0;
   virtual void DrawToWindow(sf::RenderWindow &window) = 0;
   virtual void HandleEvent(sf::Event& event, Coord mouse_position) = 0;
+  virtual void ResizeWindow(unsigned new_width, unsigned new_height) = 0;
+
+protected:
+  std::array<sf::Color,(size_t)GuiColor::SIZE> color_palette_;
 
 };
 } // namespace sfml_window
