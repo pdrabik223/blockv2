64d6662bdd43847f557c779f15a7cbd6cdcf0149
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 18:12:06 +0200

    code reformat

--- utility/rect.cpp
+++ utility/rect.cpp
@@ -5,11 +5,14 @@
 #include "rect.h"
 Rect::Rect(const Coord &position, unsigned int width, unsigned int height)
     : placement(position), width(width), height(height) {}
-    bool Rect::CheckWBoundaries(const Coord&  point) {
+
+bool Rect::CheckWBoundaries(const Coord &point) {
+
   if (point.x < placement.x || point.y < placement.y)
     return false;
   if (point.x - placement.x > width || point.y - placement.y > height)
     return false;
   return true;
 }
-Rect::Rect(): placement(), width(0), height(0) {}
+
+Rect::Rect() : placement(), width(0), height(0) {}
