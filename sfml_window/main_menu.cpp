dd622040bee94cfe5a57c37639b0d05d1f19b696
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-01 13:44:49 +0200

    buttons and colors

--- sfml_window/main_menu.cpp
+++ sfml_window/main_menu.cpp
@@ -4,25 +4,50 @@
 
 #include "main_menu.h"
 
-sfml_window::MainMenu::MainMenu() {
+sfml_window::MainMenu::MainMenu(unsigned int window_width,
+                                unsigned int window_height)
+    : window_width_(window_width), window_height_(window_height) {
+  LoadColors();
   LoadButtons();
 }
+
 sfml_window::MainMenu::~MainMenu() {
   for (auto &button : buttons_)
     delete button;
 }
 void sfml_window::MainMenu::LoadButtons() {
-  buttons_[(unsigned)MainMenuButton::EXIT] = new TextButton(Rect({0, 0},100,100),std::string("cos"));
-  buttons_[(unsigned)MainMenuButton::PLAY_LEVEL] = new TextButton(Rect({200, 200},200,100),std::string("cos"));
-  buttons_[(unsigned)MainMenuButton::CREATE_LEVEL] = new TextButton(Rect({400, 200},200,100),std::string("cos"));
-  buttons_[(unsigned)MainMenuButton::MULTIPLAYER] = new TextButton(Rect({400, 100},200,100),std::string("cos"));
-  buttons_[(unsigned)MainMenuButton::CREDITS] = new TextButton(Rect({200, 300},200,100),std::string("cos"));
 
+  buttons_[(unsigned)MainMenuButton::EXIT] =
+      new TextButton({{window_width_ - 100, 0}, 100, 100}, std::string("EXIT"));
+  buttons_[(unsigned)MainMenuButton::EXIT]->SetButtonColor(
+      color_palette_[(unsigned)GuiColor::DANGER_COLOR]);
+
+  buttons_[(unsigned)MainMenuButton::PLAY_LEVEL] = new TextButton(
+      AlignCenter({{0, 100}, 400, 100}), std::string("Play level"));
+  buttons_[(unsigned)MainMenuButton::PLAY_LEVEL]->SetButtonColor(
+      color_palette_[(unsigned)GuiColor::MENU_PRIMARY_COLOR]);
+
+  buttons_[(unsigned)MainMenuButton::CREATE_LEVEL] = new TextButton(
+      AlignCenter({{0, 250}, 400, 100}), std::string("Create level"));
+  buttons_[(unsigned)MainMenuButton::CREATE_LEVEL]->SetButtonColor(
+      color_palette_[(unsigned)GuiColor::MENU_PRIMARY_COLOR]);
+
+  buttons_[(unsigned)MainMenuButton::MULTIPLAYER] = new TextButton(
+      AlignCenter({{0, 400}, 400, 100}), std::string("Multiplayer mode"));
+  buttons_[(unsigned)MainMenuButton::MULTIPLAYER]->SetButtonColor(
+      color_palette_[(unsigned)GuiColor::MENU_PRIMARY_COLOR]);
+
+  buttons_[(unsigned)MainMenuButton::CREDITS] =
+      new TextButton(AlignCenter({{0, 550}, 400, 100}), std::string("credits"));
+
+  buttons_[(unsigned)MainMenuButton::CREDITS]->SetButtonColor(
+      color_palette_[(unsigned)GuiColor::INFORMATIVE_COLOR]);
 }
 
 void sfml_window::MainMenu::DrawToWindow(sf::RenderWindow &window) {
-  window.clear(background_color_);
+  window.clear(color_palette_[(unsigned)GuiColor::MENU_BACKGROUND_COLOR]);
   for (const auto &button : buttons_)
+
     button->DrawToWindow(window);
 }
 void sfml_window::MainMenu::HandleEvent(sf::Event &event,
@@ -46,4 +71,27 @@ void sfml_window::MainMenu::HandleEvent(sf::Event &event,
         // switch context to dispaly credits
         break;
       }
+}
+void sfml_window::MainMenu::ResizeWindow(unsigned int new_width,
+                                         unsigned int new_height) {
+  window_width_ = new_width;
+  window_height_ = new_height;
+}
+Rect sfml_window::MainMenu::AlignCenter(Rect default_position) {
+
+  default_position.placement.x =
+      (window_width_ / 2) - default_position.width / 2;
+  return default_position;
+}
+void sfml_window::MainMenu::LoadColors() {
+    color_palette_[(unsigned)GuiColor::MENU_PRIMARY_COLOR] = sf::Color(0x00ff99);
+    color_palette_[(unsigned)GuiColor::MENU_SECONDARY_COLOR] = sf::Color(0x9800ff);
+    color_palette_[(unsigned)GuiColor::MENU_TERTIARY_COLOR] = sf::Color(0xff9900);
+    color_palette_[(unsigned)GuiColor::MENU_BACKGROUND_COLOR] = sf::Color(239,206,206);
+    color_palette_[(unsigned)GuiColor::DANGER_COLOR] = sf::Color(255,0,0);
+    color_palette_[(unsigned)GuiColor::WARNING_COLOR] = sf::Color(255,255,0);
+    color_palette_[(unsigned)GuiColor::INFORMATIVE_COLOR] = sf::Color(0,0,255);
+    color_palette_[(unsigned)GuiColor::SAFE_COLOR] = sf::Color(0,255,0);
+
+
 }
--- sfml_window/main_menu.h
+++ sfml_window/main_menu.h
@@ -12,7 +12,7 @@
 #include <vector>
 #include <array>
 namespace sfml_window {
-/// white background
+
 enum class MainMenuButton {
   EXIT,
   PLAY_LEVEL,
@@ -21,22 +21,40 @@ enum class MainMenuButton {
   CREDITS,
   SIZE
 };
-
+/// first screen player sees after lunching game
+/// contains basic info as well as
 class MainMenu : public Context {
 public:
-  MainMenu();
+  MainMenu(unsigned int window_width, unsigned int window_height);
   void DrawToWindow(sf::RenderWindow &window) override;
   void HandleEvent(sf::Event &event, Coord mouse_position) override;
-  virtual ~MainMenu();
+
+  void ResizeWindow(unsigned int new_width, unsigned int new_height) override;
+
+  ~MainMenu();
 
 private:
+public:
+  void LoadColors() override;
+
+private:
+  Rect AlignCenter(Rect default_position);
   void LoadButtons();
+  void DeleteButtons();
 
 protected:
   std::array<Button *, (unsigned)MainMenuButton::SIZE> buttons_;
   TextBox game_name_ = {{0, 0},"Block v2"};
   TextBox author_name_ = {{100,100},"author: piotr233"};
-  sf::Color background_color_ = sf::Color(220,0,0);
+
+
+  /// \format in pixels
+  /// x axis domain = <0,window_width_>
+  unsigned window_width_;
+
+  /// \format in pixels
+  /// y axis domain = <0,window_height_>
+  unsigned window_height_;
 };
 } // namespace sfml_window
 #endif // BLOCK_V2_SFML_WINDOW_MAIN_MENU_H_
