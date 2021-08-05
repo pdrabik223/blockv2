d1d6b4c03537efbbaff463bd8b1cd45b3d6d98fa
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-05 21:22:11 +0200

    randomize bots function

--- test.cpp
+++ test.cpp
@@ -5,14 +5,50 @@
 #include "bot.h"
 #include "level_info.h"
 #include "sfml_window/window.h"
+void FillWithBots(LevelInfo &target_level, double fill_level);
 int main() {
+  srand(time(NULL));
+  LevelInfo game(20, 10);
 
-  LevelInfo game(10,5);
-  game.AddBot({3, 4},BotType::GOAL);
- // game.AddBot({4, 4},BotType::ENGINE,Direction::RIGHT);
+  FillWithBots(game, 0.5);
+  //  game.AddBot({0, 0}, BotType::GOAL);
+  // game.AddBot({4, 4},BotType::ENGINE,Direction::RIGHT);
   game.SaveLevel();
   sfml_window::Gui sin(game);
 
-
   return 0;
+}
+
+void FillWithBots(LevelInfo &target_level, double fill_level = 0.3) {
+  for (unsigned y = 0; y < target_level.GetHeight(); y++)
+    for (unsigned x = 0; x < target_level.GetWidth(); x++)
+      if (rand() > fill_level)
+        switch (rand() % 8) {
+        case 0:
+          target_level.AddBot({x, y}, BotType::BASIC);
+          break;
+        case 1:
+          target_level.AddBot({x, y}, BotType::BEDROCK);
+          break;
+        case 2:
+          target_level.AddBot({x, y}, BotType::TURN,
+                              (TurnDirection)(rand() % 2));
+          break;
+        case 3:
+          target_level.AddBot({x, y}, BotType::GOAL);
+          break;
+        case 4:
+          target_level.AddBot({x, y}, BotType::ENEMY);
+          break;
+        case 5:
+          target_level.AddBot({x, y}, BotType::ENGINE, (Direction)(rand() % 4));
+          break;
+        case 6:
+          target_level.AddBot({x, y}, BotType::FACTORY,
+                              (Direction)(rand() % 4));
+          break;
+        case 7:
+          target_level.AddBot({x, y}, BotType::TP, rand());
+          break;
+        }
 }
