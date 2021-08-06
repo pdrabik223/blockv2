47a899c8871804eb392e52f043c7517ebdeda400
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-06 16:16:49 +0200

    header and unnecessary spaces

--- sfml_window/window_utility/gui_color.cpp
+++ sfml_window/window_utility/gui_color.cpp
@@ -6,9 +6,7 @@
 
 sf::Color Light(const sf::Color &color) {
   sf::Color light_color = color;
-
   int upper_break_point = 255 - 90;
-
   light_color.r = light_color.r > upper_break_point ? 255 : light_color.r + 90;
   light_color.g = light_color.g > upper_break_point ? 255 : light_color.g + 90;
   light_color.b = light_color.b > upper_break_point ? 255 : light_color.b + 90;
--- sfml_window/window_utility/gui_color.h
+++ sfml_window/window_utility/gui_color.h
@@ -2,12 +2,12 @@
 // Created by studio25 on 01.08.2021.
 //
 
-#ifndef BLOCK_V2_UTILITY_GUI_COLOR_H_
-#define BLOCK_V2_UTILITY_GUI_COLOR_H_
+#ifndef BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_GUI_COLOR_H_
+#define BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_GUI_COLOR_H_
 #include "SFML/Graphics/Color.hpp"
 
 /// \param color primary color, to be lighten
 /// \return light/ brighter  version of the primary color
 sf::Color Light(const sf::Color& color);
 
-#endif // BLOCK_V2_UTILITY_GUI_COLOR_H_
+#endif // BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_GUI_COLOR_H_
