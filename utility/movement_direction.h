1eef6b189851cc0eaf87791b21208f2c3570ab3f
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-08 02:08:43 +0200

    movement direction truth table and + sign operator

--- utility/movement_direction.h
+++ utility/movement_direction.h
@@ -4,20 +4,58 @@
 
 #ifndef BLOCK_V2_UTILITY_MOVEMENT_DIRECTION_H_
 #define BLOCK_V2_UTILITY_MOVEMENT_DIRECTION_H_
+#include <vector>
 namespace movement_direction {
+
 enum class Direction {
-  NONE,
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
-  LOCK_HORIZONTAL,
-  LOCK_VERTICAL,
-  LOCK_GLOBAL,
+  NONE = 0,
+  UP = 1,
+  DOWN = 2,
+  LEFT = 3,
+  RIGHT = 4,
+  LOCK_HORIZONTAL = 5,
+  LOCK_VERTICAL = 6,
+  LOCK_GLOBAL = 7,
   SIZE
 };
+const Direction truth_table[(unsigned long long)Direction::SIZE][(
+    unsigned long long)Direction::SIZE] = {
+    {Direction::NONE, Direction::UP, Direction::DOWN, Direction::LEFT,
+     Direction::RIGHT, Direction::LOCK_HORIZONTAL, Direction::LOCK_VERTICAL,
+     Direction::LOCK_GLOBAL},
+     {Direction::UP, Direction::UP, Direction::LOCK_VERTICAL, Direction::LEFT,
+      Direction::UP, Direction::UP, Direction::LOCK_VERTICAL,
+      Direction::LOCK_GLOBAL},
+      {Direction::DOWN, Direction::LOCK_VERTICAL, Direction::DOWN,
+       Direction::DOWN, Direction::RIGHT, Direction::DOWN,
+       Direction::LOCK_VERTICAL, Direction::LOCK_GLOBAL},
+       {Direction::LEFT, Direction::LEFT, Direction::DOWN, Direction::LEFT,
+        Direction::LOCK_HORIZONTAL, Direction::LOCK_HORIZONTAL, Direction::LEFT,
+        Direction::LOCK_GLOBAL},
+        {Direction::RIGHT, Direction::UP, Direction::RIGHT,
+         Direction::LOCK_HORIZONTAL, Direction::RIGHT, Direction::LOCK_HORIZONTAL,
+         Direction::RIGHT, Direction::LOCK_GLOBAL},
+         {Direction::LOCK_HORIZONTAL, Direction::UP, Direction::DOWN,
+          Direction::LOCK_HORIZONTAL, Direction::LOCK_HORIZONTAL,
+          Direction::LOCK_HORIZONTAL, Direction::LOCK_GLOBAL,
+          Direction::LOCK_GLOBAL},
+          {Direction::LOCK_VERTICAL, Direction::LOCK_VERTICAL,
+           Direction::LOCK_VERTICAL, Direction::LEFT, Direction::RIGHT,
+           Direction::LOCK_GLOBAL, Direction::LOCK_VERTICAL, Direction::LOCK_GLOBAL},
+           {Direction::LOCK_GLOBAL, Direction::LOCK_GLOBAL, Direction::LOCK_GLOBAL,
+            Direction::LOCK_GLOBAL, Direction::LOCK_GLOBAL, Direction::LOCK_GLOBAL,
+            Direction::LOCK_GLOBAL, Direction::LOCK_GLOBAL}};
+///
+/// \param a first argument
+/// \param b second argument
+/// \return sum of it's arguments, following the <a href="../README.md">truth table</a>
 
-Direction &operator+(const Direction &other);
+Direction operator+(const Direction &a, const Direction &b) {
+  return truth_table[(unsigned)a][(unsigned)b];
+}
+
+
+
 
 } // namespace movement_direction
 #endif // BLOCK_V2_UTILITY_MOVEMENT_DIRECTION_H_
