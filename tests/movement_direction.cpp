3b982ec36d60017a8fca4d228964f7d784ec98fd
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-08 02:08:06 +0200

    new testing file

---
+++ tests/movement_direction.cpp
@@ -0,0 +1,22 @@
+//
+// Created by studio25 on 08.08.2021.
+//
+#include "movement_direction.h"
+#include "gtest/gtest.h"
+
+using namespace movement_direction;
+TEST(movement_dir, add_operator) {
+
+  EXPECT_TRUE( Direction::UP + Direction::UP == Direction::UP);
+
+  EXPECT_TRUE( Direction::DOWN + Direction::DOWN == Direction::DOWN);
+
+  EXPECT_TRUE( Direction::DOWN + Direction::UP == Direction::LOCK_VERTICAL);
+
+  EXPECT_TRUE( Direction::UP + Direction::LEFT == Direction::LEFT);
+
+  EXPECT_TRUE( Direction::LOCK_HORIZONTAL + Direction::LOCK_VERTICAL == Direction::LOCK_GLOBAL);
+
+
+
+}
