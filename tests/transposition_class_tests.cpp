9e674fc56478cc921314b347a7d47693415eeedc
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-11 12:10:17 +0200

    transposition class changed so did test

--- tests/transposition_class_tests.cpp
+++ tests/transposition_class_tests.cpp
@@ -8,16 +8,6 @@
 
 TEST(Transposition,Get_Movement_Direction){
 
-  Transposition trans(11, 1, new Engine, 10);
-  EXPECT_TRUE(trans.GetMovementDirection() == Direction::UP);
 
-  Transposition trans_1(1, 11, new Engine, 10);
-  EXPECT_TRUE(trans_1.GetMovementDirection() == Direction::DOWN);
-
-  Transposition trans_2(1, 2, new Engine, 10);
-  EXPECT_TRUE(trans_2.GetMovementDirection() == Direction::LEFT);
-
-  Transposition trans_3(2, 1, new Engine, 10);
-  EXPECT_TRUE(trans_3.GetMovementDirection() == Direction::RIGHT);
 
 }
