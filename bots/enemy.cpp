b6a8f6d7bf85af798ef358cf087c9f6e5c9c14f8
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-12 13:43:34 +0200

    enemy almost works

--- bots/enemy.cpp
+++ bots/enemy.cpp
@@ -15,7 +15,28 @@ void Enemy::CalculateMovementDirection(const std::vector<Bot *> &plane,
                                        const Coord &bot_position,
                                        const unsigned plane_width,
                                        const unsigned plane_height,
-                                       const Direction push_direction) {}
+                                       const Direction push_direction) {
+
+  Coord new_position = NextPosition(push_direction, bot_position);
+  Coord pusher_position = NextPosition(Opposite(push_direction), bot_position);
+
+  // if true the block behaves like basic block
+  if(plane[pusher_position.ToInt(plane_width)]->GetType() == BotType::BASIC) {
+
+    plane[new_position.ToInt(plane_width)]->CalculateMovementDirection(
+        plane, new_position, plane_width, plane_height, push_direction);
+    // if the next cell is "pushable" in the push_direction
+    // this cell is pushable also in the push_direction
+    if (plane[new_position.ToInt(plane_width)]->GetMovement().CheckDirection(
+    	push_direction)) {
+      movement_.AddDirection(push_direction);
+      movement_.LockEdge(Opposite(push_direction));
+    } else
+      movement_.LockEdge(push_direction);
+
+  }else movement_.LockEdge(push_direction);
+
+}
 
 void Enemy::ClearMovementDirection() { movement_.Clear(); }
 void Enemy::Action(const std::vector<Bot *> &plane, const Coord &bot_position,
