250de4cd1516d38120314081b6be7b7ea6e73b14
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-14 12:44:07 +0200

    filling in the remaining contexts

--- sfml_window/context/level_picker.h
+++ sfml_window/context/level_picker.h
@@ -5,6 +5,33 @@
 #ifndef BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
 #define BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
 
-class LevelPicker {};
+/// displays the content of the levels directory, and enables user to choose
+/// witch level they want to play
 
+namespace sfml_window {
+enum class LevelPickerButton {
+  EXIT,
+  SIZE
+};
+class LevelPicker {
+public:
+  /// \format in pixels
+  /// x axis domain = <0,window_width_>
+  unsigned window_width_;
+
+  /// \format in pixels
+  /// y axis domain = <0,window_height_>
+  unsigned window_height_;
+
+  /// background texture
+  sf::Texture background_texture_;
+  /// background sprite always provided by user
+  sf::Sprite background_sprite_;
+
+  std::array<Button *, (unsigned)LevelPickerButton::SIZE> buttons_;
+
+
+
+};
+}
 #endif // BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
