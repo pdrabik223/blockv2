f4fdd9018240fea560a0c61553b0f810e3ffbc59
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-14 12:50:25 +0200

    the class compiles

--- sfml_window/context/level_picker.h
+++ sfml_window/context/level_picker.h
@@ -5,15 +5,25 @@
 #ifndef BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
 #define BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
 
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include "context.h"
+#include "text_button.h"
 /// displays the content of the levels directory, and enables user to choose
 /// witch level they want to play
 
 namespace sfml_window {
-enum class LevelPickerButton {
-  EXIT,
-  SIZE
-};
-class LevelPicker {
+enum class LevelPickerButton { EXIT, SIZE };
+class LevelPicker : public Context {
+
+  void LoadColors() {}
+  void DrawToWindow(sf::RenderWindow &window) {}
+  sfml_window::ContextEvent HandleEvent(sf::Event &event,
+                                        const sf::RenderWindow &window) {
+    return sfml_window::ContextEvent::NONE;
+  }
+
 public:
   /// \format in pixels
   /// x axis domain = <0,window_width_>
@@ -29,9 +39,6 @@ public:
   sf::Sprite background_sprite_;
 
   std::array<Button *, (unsigned)LevelPickerButton::SIZE> buttons_;
-
-
-
 };
-}
+} // namespace sfml_window
 #endif // BLOCK_V2_SFML_WINDOW_CONTEXT_LEVEL_PICKER_H_
