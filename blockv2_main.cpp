654f538fcf94d9fcf0fe84bfd3ab4b7f6fa6821a
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-14 13:13:35 +0200

    new executable showing whole application

--- blockv2_main.cpp
+++ blockv2_main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by studio25 on 14.08.2021.
 //
+#include "bot.h"
+#include "level_info.h"
+#include "sfml_window/window.h"
+
+int main() {
+
+  sfml_window::Gui sin;
+
+  return 0;
+
+}
