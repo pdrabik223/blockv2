119fcf93814b127017dfbe05231354fe4a0926e1
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-15 14:17:56 +0200

    buttons for run sim and exit

--- sfml_window/context/level_player.cpp
+++ sfml_window/context/level_player.cpp
@@ -13,3 +13,16 @@ sfml_window::LevelPlayer::LevelPlayer(unsigned int window_width,
 void sfml_window::LevelPlayer::DrawToWindow(sf::RenderWindow &window) {
 
 }
+
+void sfml_window::LevelPlayer::LoadButtons() {
+  std::string directory = "../sfml_window/assets/level_player/";
+  buttons_[(unsigned)LevelPlayerButton::EXIT] =
+      new ImageButton(Rect(Coord(window_width_ - 36, 4), 32, 32),
+                      directory + "cancel-button.png",
+                      color_palette_[(unsigned)GuiColor::DANGER_COLOR]);
+
+  buttons_[(unsigned)LevelPlayerButton::RUN_SIMULATION] =
+      new ImageButton(Rect(Coord(window_width_ - 74, 4), 32, 32),
+          directory + "run-button.png",
+           color_palette_[(unsigned)GuiColor::INFORMATIVE_COLOR]);
+}
