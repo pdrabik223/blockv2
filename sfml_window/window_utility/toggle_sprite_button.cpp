48edc9e86e59fe90c10150856bc7b0dba5b84625
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-17 17:49:59 +0200

    this button is working

--- sfml_window/window_utility/toggle_sprite_button.cpp
+++ sfml_window/window_utility/toggle_sprite_button.cpp
@@ -16,7 +16,6 @@ sfml_window::ToggleSpriteButton::ToggleSpriteButton(const Rect &structure,
       (float)structure.height / (float)sprite_.first.getSize().y);
   sprite_.second.setPosition((float)structure.placement.x,
                              (float)structure.placement.y);
-
 }
 
 void sfml_window::ToggleSpriteButton::TurnOn() { state_ = State::TOGGLE; }
@@ -25,9 +24,11 @@ void sfml_window::ToggleSpriteButton::TurnOff() { state_ = State::DEFAULT; }
 
 bool sfml_window::ToggleSpriteButton::DetectInteraction(
     const Coord &press_point, sf::Event &event) {
-  bool hover = structure_.CheckWBoundaries(press_point);
 
-  state_ = state_ == State::TOGGLE ? State::DEFAULT : State::TOGGLE;
+  bool hover = structure_.CheckWBoundaries(press_point);
+  if (event.type == sf::Event::MouseButtonReleased &&
+      event.mouseButton.button == sf::Mouse::Left)
+    state_ = state_ == State::TOGGLE ? State::DEFAULT : State::TOGGLE;
 
   return hover && (event.type == sf::Event::MouseButtonReleased &&
                    event.mouseButton.button == sf::Mouse::Left);
@@ -42,7 +43,6 @@ bool sfml_window::ToggleSpriteButton::DetectHover(const Coord &press_point) {
   return change;
 }
 void sfml_window::ToggleSpriteButton::DrawToWindow(sf::RenderWindow &window) {
-
 
   if (hover_ or state_ == State::TOGGLE)
     sprite_.second.setColor({255, 255, 255, 255});
