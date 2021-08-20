5b5001b53c69d25f608fd1b36b8a7064ba97505c
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-08-20 16:48:35 +0200

    toggle text button class, filled in with basic functions, not yet working

---
+++ sfml_window/window_utility/toggle_text_button.cpp
@@ -0,0 +1,88 @@
+//
+// Created by studio25 on 20.08.2021.
+//
+
+#include "toggle_text_button.h"
+
+bool sfml_window::ToggleTextButton::DetectInteraction(const Coord &press_point,
+                                                sf::Event &event) {
+  hover_ = structure_.CheckWBoundaries(press_point);
+
+  return hover_ && (event.type == sf::Event::MouseButtonReleased &&
+  event.mouseButton.button == sf::Mouse::Left);
+}
+
+bool sfml_window::ToggleTextButton::DetectHover(const Coord &press_point) {
+  bool change =
+      hover_ not_eq structure_.CheckWBoundaries(press_point) ? true : false;
+  hover_ = structure_.CheckWBoundaries(press_point);
+  return change;
+}
+void sfml_window::ToggleTextButton::DrawToWindow(sf::RenderWindow &window) {
+
+  if (hover_)
+    window.draw(background_);
+
+  text_.DrawToWindow(window);
+}
+
+void sfml_window::ToggleTextButton::SetButtonColor(const sf::Color &button_color) {
+  button_color_ = button_color;
+  background_.setFillColor(Light(button_color_));
+  background_.setOutlineColor(button_color_);
+}
+sfml_window::ToggleTextButton::ToggleTextButton(const Rect &structure,
+                                    const std::string &text,
+                                    const sf::Color &button_color,
+                                    bool display_background = false)
+                                    : structure_(structure), text_(structure, text, button_color_),
+                                    button_color_(button_color), display_background_(display_background),
+                                    hover_(false) {
+
+  background_.setFillColor(Light(button_color_));
+  background_.setOutlineColor(button_color_);
+
+  background_.setPosition((float)structure_.placement.x,
+                          (float)structure_.placement.y);
+
+  background_.setSize({(float)structure_.width, (float)structure_.height});
+  text_.SetFontSize(structure);
+}
+sfml_window::ToggleTextButton::ToggleTextButton(const Coord &position,
+                                    const std::string &text,
+                                    const sf::Color &button_color,
+                                    bool display_background,
+                                    unsigned int font_size)
+                                    : text_(position, text, button_color_, font_size),
+                                    button_color_(button_color), display_background_(display_background),
+                                    hover_(false) {
+
+  background_.setFillColor(Light(button_color_));
+  background_.setOutlineColor(button_color_);
+
+  structure_ = text_.GetFontBoundaries();
+  background_.setSize({(float)structure_.width, (float)structure_.height});
+
+  background_.setPosition((float)structure_.placement.x,
+                          (float)structure_.placement.y);
+
+  background_.setSize({(float)structure_.width, (float)structure_.height});
+}
+sfml_window::ToggleTextButton::ToggleTextButton(const sfml_window::ToggleTextButton& other) {
+  structure_ = other.structure_;
+  background_ = other.background_;
+  text_ = other.text_;
+  hover_ = other.hover_;
+  button_color_ = other.button_color_;
+  display_background_ = other.display_background_;
+}
+void sfml_window::ToggleTextButton::SetPosition(const Coord &position) {
+
+  structure_.SetPlacement(position);
+  background_.setPosition((float)structure_.placement.x,
+                          (float)structure_.placement.y);
+  text_.SetPosition(position);
+}
+sfml_window::ToggleTextButton *sfml_window::ToggleTextButton::Clone() { return new ToggleTextButton(*this); }
+
+sfml_window::ToggleTextButton::~ToggleTextButton() = default;
---
+++ sfml_window/window_utility/toggle_text_button.h
@@ -0,0 +1,64 @@
+//
+// Created by studio25 on 20.08.2021.
+//
+
+#ifndef BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_TOGGLE_TEXT_BUTTON_H_
+#define BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_TOGGLE_TEXT_BUTTON_H_
+
+
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include "button.h"
+#include "text_box.h"
+ namespace sfml_window {
+/// the size of the button depends on the text within
+class ToggleTextButton : public Button {
+  ToggleTextButton() = default;
+  ToggleTextButton(const ToggleTextButton& other);
+
+  ///
+  /// \param structure  the position on screen and boundaries the text will fill
+  /// \param text displayed text
+  /// \param button_color color of the text
+  /// \param display_background true if the background should be displayed, false if not
+  ToggleTextButton(const Rect &structure, const std::string &text,
+             const sf::Color &button_color, bool display_background);
+
+  /// \param position  the position on screen, actual size of button will be set to the size of text
+  /// \param text displayed text
+  /// \param button_color color of the text
+  /// \param display_background true if the background should be displayed, false if not
+  /// \param font_size the size of button letters
+  ToggleTextButton(const Coord &position,const std::string &text, const sf::Color &button_color,
+             bool display_background,unsigned font_size);
+
+  /// updates look of a button depending on the position of mouse
+  /// \param press_point current mouse position on screen
+  /// \param event last window event
+  /// \return true if button is pressed
+  bool DetectInteraction(const Coord &press_point, sf::Event &event) override;
+
+  bool DetectHover(const Coord &press_point) override;
+
+  /// display current look of a button to window
+  /// \param window target to display button to
+  void DrawToWindow(sf::RenderWindow &window) override;
+  void SetButtonColor(const sf::Color &button_color) override;
+  void SetPosition(const Coord& position);
+  ToggleTextButton *Clone() override;
+  ~ToggleTextButton() override;
+
+protected:
+  Rect structure_;
+  sf::RectangleShape background_;
+  TextBox text_;
+
+  /// if mouse is hovering over the button the font is larger
+  bool hover_ = false;
+  sf::Color button_color_;
+  bool display_background_ = false ;
+
+  };
+}
+#endif // BLOCK_V2_SFML_WINDOW_WINDOW_UTILITY_TOGGLE_TEXT_BUTTON_H_
